90f4ce4eca74799afeeea2765bb54ea447efecb0
Zachary Dennett <zach.dennett@gmail.com>
2012-10-21 09:14:59 -0400

    Playing with flight model. Committing a version that works for cabin choice, but doesnt seem efficient

---
+++ flightsite/flights/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from flights.models import Flight
+
+admin.site.register(Flight)
--- flightsite/flights/models.py
+++ flightsite/flights/models.py
@@ -5,12 +5,25 @@ class Flight(models.Model):
     origin = models.CharField(max_length=3)
     destination = models.CharField(max_length=3)
     distance = models.FloatField()
-    flight_date = models.DateTimeField('flight date')
-    airline_code = models.CharField(max_length=3)
-    cabin = models.CharField(max_length=20)
+    flight_date = models.DateField('flight date')
+    airline = models.CharField(max_length=50)
+    
+    # Cabin field includes choices
+    F = 'Frst'
+    Y = 'Ecnmy'
+    cabin_choices = (
+        (F, 'First Class'),
+        (Y,'Economy'))
+    cabin = models.CharField(max_length=20,
+                            choices = cabin_choices,
+                            default = Y)
+    
     equipment = models.CharField(max_length=50)
     miles_redemption = models.BooleanField()
-    ## To do, make equipment a choosable field https://docs.djangoproject.com/en/dev/ref/models/fields/#django.db.models.choices
-    
+
     def __unicode__(self):
-        return self.origin + self.destination
+        return self.origin + "-" + self.destination + " " + str(self.flight_date.month) + "/" +str(self.flight_date.day) +"/" + str(self.flight_date.year)
+
+
+## To do, make equipment a choosable field https://docs.djangoproject.com/en/dev/ref/models/fields/#django.db.models.choices
+
