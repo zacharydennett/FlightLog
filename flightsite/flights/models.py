684b11657be8f222155a25312fdf7a120cf0ccbe
Zachary Dennett <zach.dennett@gmail.com>
2012-10-21 08:36:37 -0400

    Finished first draft of flights model
    Still lots of work to do on it. For example, want equipment, cabin,
    airline, to be a list of choices. Need distance to calculate

--- flightsite/flights/models.py
+++ flightsite/flights/models.py
@@ -4,8 +4,13 @@ from django.db import models
 class Flight(models.Model):
     origin = models.CharField(max_length=3)
     destination = models.CharField(max_length=3)
+    distance = models.FloatField()
     flight_date = models.DateTimeField('flight date')
     airline_code = models.CharField(max_length=3)
     cabin = models.CharField(max_length=20)
-    equipment = models.CharField(max_length=50)('aircraft type')
-    miles_redemption = modles.binary
+    equipment = models.CharField(max_length=50)
+    miles_redemption = models.BooleanField()
+    ## To do, make equipment a choosable field https://docs.djangoproject.com/en/dev/ref/models/fields/#django.db.models.choices
+    
+    def __unicode__(self):
+        return self.origin + self.destination
