207d8831df8d8dc97daa8b298e2835adf182e64e
Zachary Dennett <zach.dennett@gmail.com>
2012-10-23 11:17:03 -0400

    Defined a custom function on flights for distance. (Not yet working)

--- flightsite/flights/admin.py
+++ flightsite/flights/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from flights.models import Flight, Aircraft_list, Airport_list, Airline_list
+from flights.models import Flight, Aircraft_list, Airport_origin_list, Airline_list
 
 admin.site.register(Flight)
 admin.site.register(Aircraft_list)
-admin.site.register(Airport_list)
+admin.site.register(Airport_origin_list)
 admin.site.register(Airline_list)
--- flightsite/flights/models.py
+++ flightsite/flights/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 
+class Airport_origin_list(models.Model):
+    airport_code = models.CharField(max_length=3)
+    airport_name = models.CharField(max_length=100)
+    longitude = models.FloatField()
+    def __unicode__(self):
+        return self.airport_code + "-" + self.airport_name
+    class Meta:
+        ordering = ('airport_code',)
+
+
 # Flight module holds flight level information
 class Flight(models.Model):
-    origin = models.ForeignKey('Airport_list')
-    # destination = models.ForeignKey('Airport_list')
-    distance = models.FloatField()
+    origin = models.ForeignKey(Airport_origin_list)
+    destination = models.CharField(max_length=10)
     flight_date = models.DateField('flight date')
     airline = models.ForeignKey('Airline_list')
     
@@ -20,12 +29,12 @@ class Flight(models.Model):
                             choices = cabin_choices[1],
                             default = cabin_choices[1][3]
                             )
-    
     aircraft = models.ForeignKey('Aircraft_list')
     miles_redemption = models.BooleanField()
     def __unicode__(self):
-        return self.origin + "-" + self.destination + " " + str(self.flight_date.month) + "/" +str(self.flight_date.day) +"/" + str(self.flight_date.year)
-
+        return str(self.origin) + "-" + str(self.destination) + " " + str(self.flight_date.month) + "/" +str(self.flight_date.day) +"/" + str(self.flight_date.year)
+    def flight_distance(self):
+        return self.origin.longitude ## TO DO make this actually calculate distance!
 
 # This table is createds to define the choices of aircraft
 class Aircraft_list(models.Model):
@@ -34,14 +43,6 @@ class Aircraft_list(models.Model):
         return self.aircraft_name
     class Meta:
         ordering = ('aircraft_name',)
-
-class Airport_list(models.Model):
-    airport_code = models.CharField(max_length=3)
-    airport_name = models.CharField(max_length=100)
-    def __unicode__(self):
-        return self.airport_code + "-" + self.airport_name
-    class Meta:
-        ordering = ('airport_code',)
 
 class Airline_list(models.Model):
     airline_name = models.CharField(max_length=100)
