9ffd77d885ab50823c8d32716e235ebb87743910
Zachary Dennett <zach.dennett@gmail.com>
2012-10-23 11:46:34 -0400

    Models working with exception of distance calculation

--- flightsite/flights/models.py
+++ flightsite/flights/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
 class Airport_list(models.Model):
     airport_code = models.CharField(max_length=3)
     airport_name = models.CharField(max_length=100)
     longitude = models.FloatField()
+    latitude = models.FloatField()
     def __unicode__(self):
         return self.airport_code + "-" + self.airport_name
     class Meta:
@@ -17,18 +17,13 @@ class Flight(models.Model):
     destination = models.ForeignKey(Airport_list, related_name = "destination_key")
     flight_date = models.DateField('flight date')
     airline = models.ForeignKey('Airline_list')
-    
-    # Cabin field includes choices
     cabin_choices = (('Cabin', (
                         ('first', 'First Class'),
                         ('business', 'Business Class'),
                         ('economy plus', 'Economy Plus Class'),
                         ('economy', 'Economy Class'),
                       )))
-    cabin = models.CharField(max_length=20,
-                            choices = cabin_choices[1],
-                            default = cabin_choices[1][3]
-                            )
+    cabin = models.CharField(max_length=20, choices = cabin_choices[1],default = cabin_choices[1][3])
     aircraft = models.ForeignKey('Aircraft_list')
     miles_redemption = models.BooleanField()
     def __unicode__(self):
@@ -36,13 +31,14 @@ class Flight(models.Model):
     def flight_distance(self):
         return self.origin.longitude + self.destination.longitude ## TO DO make this actually calculate distance!
 
-# This table is createds to define the choices of aircraft
+
 class Aircraft_list(models.Model):
     aircraft_name = models.CharField(max_length=30)
     def __unicode__(self):
         return self.aircraft_name
     class Meta:
         ordering = ('aircraft_name',)
+
 
 class Airline_list(models.Model):
     airline_name = models.CharField(max_length=100)
