34b8ffaf6adbf6aa605d77a254934866eac596f3
Zachary Dennett <zach.dennett@gmail.com>
2012-10-23 08:47:18 -0400

    Added ordering for *_list classes. Commented out destination until I can figure out mapping

--- flightsite/flights/models.py
+++ flightsite/flights/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Flight module holds flight level information
 class Flight(models.Model):
-    origin = models.CharField(max_length=3)
-    destination = models.CharField(max_length=3)
+    origin = models.ForeignKey('Airport_list')
+    # destination = models.ForeignKey('Airport_list')
     distance = models.FloatField()
     flight_date = models.DateField('flight date')
-    airline = models.CharField(max_length=50)
+    airline = models.ForeignKey('Airline_list')
     
     # Cabin field includes choices
     cabin_choices = (('Cabin', (
@@ -32,16 +32,22 @@ class Aircraft_list(models.Model):
     aircraft_name = models.CharField(max_length=30)
     def __unicode__(self):
         return self.aircraft_name
+    class Meta:
+        ordering = ('aircraft_name',)
 
 class Airport_list(models.Model):
     airport_code = models.CharField(max_length=3)
     airport_name = models.CharField(max_length=100)
     def __unicode__(self):
         return self.airport_code + "-" + self.airport_name
+    class Meta:
+        ordering = ('airport_code',)
 
 class Airline_list(models.Model):
     airline_name = models.CharField(max_length=100)
     airline_code = models.CharField(max_length=10)
     def __unicode__(self):
         return self.airline_code + "-" + self.airline_name
+    class Meta:
+        ordering = ('airline_name',)
     
