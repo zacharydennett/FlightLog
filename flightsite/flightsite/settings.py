357a5855aef6b8f183ecf25f883a8a40a0833598
Zachary Dennett <zach.dennett@gmail.com>
2012-10-23 14:39:11 -0400

    Changed to use a MySQL db. Getting error of missing package

--- flightsite/flightsite/settings.py
+++ flightsite/flightsite/settings.py
@@ -7,15 +7,16 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+          ('Zachary Dennett', 'zachary.denentt@gmail.com')
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/zacharydennett/Documents/GitHub/FlightLog/sqlite3.db',    # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'flightdb',    # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
