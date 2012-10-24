d5f9a4ff122c66d96c285e1ac78034fcf226b948
Zachary Dennett <zach.dennett@gmail.com>
2012-10-24 15:42:58 -0400

    remove modelutils

--- flightsite/modelutils.py
+++
@@ -1,9 +0,0 @@
-import flights.models
-from django.db import connection
-
-def add_aircraft(plane, mnf):
-    a = Aircraft_list(aircraft_name = plane, manufacturer = mnf)
-    a.save
-
-
-add_aircraft('777', 'Boeing')
