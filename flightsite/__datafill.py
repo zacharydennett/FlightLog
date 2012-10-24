3fc4a2d6b3dd035b5f5ed91d6e8043c08db85383
Zachary Dennett <zach.dennett@gmail.com>
2012-10-24 16:05:44 -0400

    Created a function add_aircraft to populate the database with aircraft choices

--- flightsite/__datafill.py
+++ flightsite/__datafill.py
@@ -1,8 +1,33 @@
-import flights.models
+from flights.models import Aircraft_list
 
-def add_aircraft(plane, mnf):
-    print plane
-#a = models.Aircraft_list(aircraft_name = plane, manufacturer = mnf)
+def add_aircraft():
+    a = Aircraft_list(aircraft_name = '737', manufacturer = 'Beoing')
+    a.save()
+    a = Aircraft_list(aircraft_name = '747', manufacturer = 'Beoing')
+    a.save()
+    a = Aircraft_list(aircraft_name = '757', manufacturer = 'Beoing')
+    a.save()
+    a = Aircraft_list(aircraft_name = '767', manufacturer = 'Beoing')
+    a.save()
+    a = Aircraft_list(aircraft_name = '777', manufacturer = 'Beoing')
+    a.save()
+    a = Aircraft_list(aircraft_name = '787', manufacturer = 'Beoing')
+    a.save()
+    a = Aircraft_list(aircraft_name = 'A319', manufacturer = 'Airbus')
+    a.save()
+    a = Aircraft_list(aircraft_name = 'A320', manufacturer = 'Airbus')
+    a.save()
+    a = Aircraft_list(aircraft_name = 'A321', manufacturer = 'Airbus')
+    a.save()
+    a = Aircraft_list(aircraft_name = 'A330', manufacturer = 'Airbus')
+    a.save()
+    a = Aircraft_list(aircraft_name = 'A340', manufacturer = 'Airbus')
+    a.save()
+    a = Aircraft_list(aircraft_name = 'A380', manufacturer = 'Airbus')
+    a.save()
+
+#print plane
+#a = models.Aircraft_list(aircraft_name = plane, amnf)
 #a.save
 
 
