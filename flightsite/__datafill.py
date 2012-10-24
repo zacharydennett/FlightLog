ed11d57b7eb047cfa03ec728f877e4d4ab380be1
Zachary Dennett <zach.dennett@gmail.com>
2012-10-24 15:42:44 -0400

    Created a file to fill data. Only a shell so far

---
+++ flightsite/__datafill.py
@@ -0,0 +1,9 @@
+import flights.models
+
+def add_aircraft(plane, mnf):
+    print plane
+#a = models.Aircraft_list(aircraft_name = plane, manufacturer = mnf)
+#a.save
+
+
+# add_aircraft('777', 'Boeing')
