ac865b48c1d4c4f4936353c28c99643f2b9ade65
Zachary Dennett <zach.dennett@gmail.com>
2012-10-28 15:55:41 -0400

    Set up hello world view for /flights/

--- flightsite/flightsite/urls.py
+++ flightsite/flightsite/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'flightsite.views.home', name='home'),
     # url(r'^flightsite/', include('flightsite.foo.urls')),
+    url(r'^flights/', include('flights.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
